de7c57e78fd88a81765b233cacd11fd7709320d6
Levi Currit <Levi.Currit@tivityhealth.com>
2021-04-29 15:58:09 -0700

    Updated Component, TW Plugins, & tsconfig src

--- craco.config.js
+++ craco.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   style: {
     postcss: {
-      plugins: [require('tailwindcss'), require('autoprefixer')],
+      plugins: [
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('postcss-nested'),
+        require('autoprefixer'),
+      ],
     },
   },
 };
