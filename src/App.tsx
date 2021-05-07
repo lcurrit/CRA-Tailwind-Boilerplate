8713ab4cecb5c06b124bfbd2bf1feab6efa8c42c
Levi Currit <levi.currit@gmail.com>
2021-05-06 17:13:11 -0700

    Helmet Testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { Route } from 'react-router-dom';
 import { Navigation, Footer } from './layout';
 import { About, Contact, Home } from './pages';
 
 function App() {
   return (
-    <>
+    <div className="application">
+      <Helmet>
+        <style type="text/css">{`
+        body {
+            background-color: blue;
+        }
+
+        p {
+            font-size: 12px;
+        }
+      `}</style>
+      </Helmet>
       <Navigation />
       <div className="container m-auto">
         <Route exact path="/" component={Home} />
@@ -13,7 +25,7 @@ function App() {
         <Route path="/contact" component={Contact} />
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
 
