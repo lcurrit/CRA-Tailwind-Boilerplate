3c3b6af73ce179f72268765b36eb342d3b8b72ca
Levi Currit <levi.currit@gmail.com>
2021-05-07 07:17:08 -0700

    Starting styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
 import { Route } from 'react-router-dom';
 import { Navigation, Footer } from './layout';
 import { About, Contact, Home } from './pages';
@@ -7,17 +6,6 @@ import { About, Contact, Home } from './pages';
 function App() {
   return (
     <div className="application">
-      <Helmet>
-        <style type="text/css">{`
-        body {
-            background-color: blue;
-        }
-
-        p {
-            font-size: 12px;
-        }
-      `}</style>
-      </Helmet>
       <Navigation />
       <div className="container m-auto">
         <Route exact path="/" component={Home} />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { Button } from '../components';
 import Title from '../components/Title';
 
 function Home() {
   return (
     <>
+      <Helmet>
+        <title>The Best Movies Ever | Home</title>
+      </Helmet>
       <Title title="Home" subtitle="it's where I keep my stuff." />
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
